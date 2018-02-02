7d1c17443b6239d0682aa2ae2a4effdb0df00603
HaydenJayAylor <35853302+HaydenJayAylor@users.noreply.github.com>
2018-02-01 23:42:35 -0800

    Add files via upload

---
+++ RoomStr.vb
@@ -0,0 +1,197 @@
+﻿Private Class Vars
+
+Dim InitCommit As String
+
+Imports System
+
+Module Mod1
+
+    Sub Load()
+        Public Shared Function Open (
+        Dim path As String = "/Users/lynn 1/Desktop/Game Dev/BONUS - w- Credits! (Converted).mov",
+        mode As FileMode,
+    ) As FileStream
+    Sub PlayBackgroundSoundFile()
+    My.Computer.Audio.Play(C:\''string of sound file location, 
+        AudioPlayMode.WaitToComplete)
+End Sub
+
+Private Sub GetNumber()
+    Dim randint As Double = Math.Ceiling( Rnd() * 20 )
+End Sub
+
+    'resource loader here
+Class Character
+
+    GetNumber()
+    Dim Name As String = randint
+    GetNumber()
+    Dim Endurance As Double = randint
+    Dim Size As String = ''Size
+    GetNumber()
+    Dim Health As Double = randint
+    GetNumber()
+    Dim BaseDamage As Double = randint
+    GetNumber()
+    Dim Stealth As Double = randint
+
+
+
+    'once player is selected
+
+
+Private Sub insertText_Click(ByVal sender As Object, ByVal e As System.EventArgs)_ 
+        Handles insertText.Click 'change this handler'
+
+        Me.displayText.Text += "Welcome to Stranger Danger"
+        Me.displayText.Text += "In classic form to play this game, please type short phrases into the command line below."
+        Me.displayText.Text += "If you type the word look, the game gives you a description of your surroundings."  
+        Me.displayText.Text += "Get, drop, and throw helps you interact with objects."
+        Me.displayText.Text += "Part of this game is trying out different commands and seeing what happens."
+        Me.displayText.Text += "Type help at any time for game instructions."
+        Me.displayText.Text += "Would you like more instructions? Type help for Help or type Begin/start/go to start the game."
+
+       'the condition handler for response'
+        Select Case Yes
+        Me.displayText.Text += "Well "
+
+        Dim Begin As String = "Begin" or "begin" or "Start" or "start" or "Go" or "go"
+
+        Select Case Begin
+        Me.displayText.Text += "You have awoken in a dark, mysterious place."
+        Me.displayText.Text += "You awaken in a dark twilight type zone. The air around you is thick, soupy "
+        Me.displayText.Text += "and has gently luminous spores floating everywhere.  There's a small alcove to the east.  "
+        Me.displayText.Text += "There is darkness in the rest of the tunnels the spores become black and ashy.  The air "
+        Me.displayText.Text += "feels malevolent dangerous … wait there is a sign it reads,"
+        Me.displayText.Text += "Welcome to the Tunnels to the Upside Down!"
+
+Dim MoreInfo As String = "The Upside Down is an alternate dimension existing in parallel to the human world. It contains the same locations and infrastructure as the human world, but it is much darker, colder and obscured by an omnipresent fog. The Upside Down is devoid of human life, instead being overgrown with ropy, root-like tendrils and biological membranes covering practically every surface. At least one recognizable animal, a humanoid creature, is native to this dimension, while ash-like spores float in the air.
+Right now you are in the tunnels under Hawkins you need to get out before you succumb to the toxic atmosphere or get sucked into the Upside Down or get hunted down by one of the many Upside Down creatures or something worse happens.  PS This room will soon be overcome by the darkness as well.
+Sincerely 1"
+    End SubPrivate Class Vars
+
+Dim InitCommit As String
+Class Room 
+Dim Room.0 As String = "Congrats you have made it out!"
+Dim Room.1 As String = "Here there is a large "
+Dim Room.2 As String = "Large Cavern connected to another room to the East"
+Dim Room.3 As String = ""
+Dim Room.4 As String = ""
+Dim Room.5 As String = ""
+Dim Room.6 As String = ""
+Dim Room.7 As String = ""
+Dim Room.8 As String = ""
+Dim Room.9 As String = ""
+Dim Room.10 As String = ""
+Dim Room.11 As String = ""
+Dim Room.12 As String = ""
+Dim Room.13 As String = ""
+Dim Room.14 As String = "You begin your adventure here in a large open cavern. There is a large corridor to the south. A narrow passage to the SW and two tunnels to the NW and NE respectively."
+Dim Room.15 As String = ""
+Dim Room.16 As String = ""
+Dim Room.17 As String = ""
+Dim Room.18 As String = ""
+Dim Room.19 As String = ""
+Dim Room.20 As String = ""
+Dim Room.21 As String = ""
+Dim Room.22 As String = ""
+Dim Room.23 As String = ""
+Dim Room.24 As String = ""
+Dim Room.25 As String = ""
+Dim Room.26 As String = ""
+Dim Room.27 As String = ""
+Dim Room.28 As String = ""
+Dim Room.29 As String = ""
+Dim Room.30 As String = ""
+Dim Room.31 As String = ""
+Dim Room.32 As String = ""
+Dim Room.33 As String = ""
+Dim Room.34 As String = ""
+Dim Room.35 As String = ""
+Dim Room.36 As String = ""
+Dim Room.37 As String = ""
+Dim Room.38 As String = "Here is a large room which open up to another room in the SE"
+Dim Room.39 As String = ""
+Dim Room.40 As String = ""
+Dim Room.41 As String = ""
+Dim Room.42 As String = ""
+Dim Room.43 As String = ""
+Dim Room.44 As String = ""
+Dim Room.45 As String = ""
+Dim Room.46 As String = ""
+Dim Room.47 As String = ""
+Dim Room.48 As String = ""
+Dim Room.49 As String = ""
+Dim Room.50 As String = ""
+Dim Room.51 As String = ""
+Dim Room.52 As String = ""
+Dim Room.53 As String = ""
+Dim Room.54 As String = ""
+Dim Room.55 As String = ""
+Dim Room.56 As String = ""
+Dim Room.57 As String = ""
+Dim Room.58 As String = ""
+Dim Room.59 As String = ""
+Dim Room.60 As String = ""
+Dim Room.61 As String = ""
+Dim Room.62 As String = ""
+Dim Room.63 As String = ""
+Dim Room.64 As String = ""
+Dim Room.65 As String = ""
+Dim Room.66 As String = ""
+Dim Room.67 As String = ""
+Dim Room.68 As String = ""
+Dim Room.69 As String = ""
+Dim Room.70 As String = ""
+Dim Room.71 As String = ""
+Dim Room.72 As String = ""
+Dim Room.73 As String = ""
+Dim Room.74 As String = ""
+Dim Room.75 As String = ""
+Dim Room.76 As String = ""
+Dim Room.77 As String = ""
+Dim Room.78 As String = ""
+Dim Room.79 As String = ""
+Dim Room.80 As String = ""
+Dim Room.81 As String = ""
+Dim Room.82 As String = ""
+Dim Room.83 As String = ""
+Dim Room.84 As String = ""
+
+End Class
+
+
+Select Case Room.14
+    Case "West"
+
+    Case "East"
+
+    Case "South"
+        Room.38
+    Case "North"
+
+    Case "NE"
+
+    Case "SE"
+
+    Case "SW"
+
+    Case "NW"
+
+Select Case Room
+    Case "West"
+
+    Case "East"
+
+    Case "South"
+
+    Case "North"
+
+    Case "NE"
+
+    Case "SE"
+
+    Case "SW"
+
+    Case "NW"
+
